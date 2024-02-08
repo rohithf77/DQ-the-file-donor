fd9d7801222c6c566bd588594bbea6340224ba8c
rohithf77 <156700198+rohithf77@users.noreply.github.com>
2024-02-09 00:08:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -98,7 +98,7 @@ async def start(client, message):
                     InlineKeyboardButton('❓ Help', callback_data='help'),
                     InlineKeyboardButton('🤖 About', callback_data='about'),
                     InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat='')
-                  ]
+                  ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
             photo=random.choice(PICS),
