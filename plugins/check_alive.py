562c7d135c8ea4bbc4d9e9569ad6e3326520322a
rohithf77 <156700198+rohithf77@users.noreply.github.com>
2024-02-21 13:38:23 +0530

    Update check_alive.py

--- plugins/check_alive.py
+++ plugins/check_alive.py
@@ -2,7 +2,7 @@ import random
 import re, asyncio, time, shutil, psutil, os, sys
 from pyrogram import Client, filters, enums
 from pyrogram.types import *
-from info.py import BOT_START_TIME, ADMINS
+from info import BOT_START_TIME, ADMINS
 from utils import humanbytes  
 
 CMD = ["/", "."]
